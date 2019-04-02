8001b1f53ab70db33b293d5685069eeec90caf14
Haposan12 <38253700+Haposan12@users.noreply.github.com>
2019-04-03 00:23:17 +0700

    Update solution2.js

--- solution2.js
+++ solution2.js
@@ -2,12 +2,13 @@ function solution(N,users) {
   var answer = [];
   var arrHasil = [];
   var hasil;
-  var pembilang = 0;
-  var penyebut = 0;
-  var map1 = new Map(); 
+  var pembilang = 0;//pembilang adalah jumlah pemain yang belum menyelesaikan stage tertentu
+  var penyebut = 0;//penyebut adalah jumlah pemain yang masuk ke stage tersebut
+  var map1 = new Map();//map untuk menampung hasil pembilang/penyebut 
 
+  //menghitung berapa penyebut dan pembilang
   for(i=1;i<=N;i++){
-    //count[];
+    
     for(j=0;j<users.length;j++){
       if(users[j] == i){
         pembilang++;
@@ -17,15 +18,19 @@ function solution(N,users) {
       }
     }
     hasil = pembilang/penyebut;
+
+    //hasil akan dimasukkan ke dalam array
     arrHasil.push(hasil);
     
+    //hasil juga dimasukkan ke dalam map dengan key i
     map1.set(i,hasil);
     pembilang = 0;
     penyebut = 0;
   }
-  arrHasil.sort();
-  arrHasil.reverse();  
-  //console.log(map1.get(1));
+  arrHasil.sort();//array hasil disort secara ascending
+  arrHasil.reverse();//array hasil yang sudah disort direverse(untuk sort descending)  
+  
+  //looping untuk mengganti hasil menjadi index stagenya
   var key;
   for(k=0;k<arrHasil.length;k++){
     for(l=1;l<=N;l++){
@@ -38,6 +43,9 @@ function solution(N,users) {
   return answer;
 } 
 
+//testing
 var user = [2,1,2,6,2,4,3,3];
 
 console.log(solution(5,user)); 
+
+//answer : [3,4,2,1,5]
